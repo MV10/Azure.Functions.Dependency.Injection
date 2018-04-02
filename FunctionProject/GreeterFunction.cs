272ac598e7cc941f7ce2ec7c5ca613f5c823acfe
Jon McGuire <mcguirev10@gmail.com>
2018-04-02 16:30:32 -0400

    migrated to functions V2, fails when deployed, see https://github.com/Azure/azure-webjobs-sdk/issues/1648

--- FunctionProject/GreeterFunction.cs
+++ FunctionProject/GreeterFunction.cs
@@ -1,10 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Azure.WebJobs.Host;
-using System;
+using System.Threading.Tasks;
 
 using FuncInjector;
 using Library;
@@ -14,53 +13,53 @@ namespace FunctionProject
     public static class GreeterFunction
     {
         [FunctionName("GreeterSingleton1")]
-        public static async Task<HttpResponseMessage> Run1(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestMessage req,
+        public static async Task<IActionResult> Run1(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
             [Inject("RegisterSingletons")] IGreeter greeter)
         {
-            return req.CreateResponse(greeter.Greet());
+            return new OkObjectResult(greeter.Greet());
         }
 
         [FunctionName("GreeterSingleton2")]
-        public static async Task<HttpResponseMessage> Run2(
-           [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestMessage req,
+        public static async Task<IActionResult> Run2(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
            [Inject("RegisterSingletons")] IGreeter greeter)
         {
-            return req.CreateResponse(greeter.Greet());
+            return new OkObjectResult(greeter.Greet());
         }
 
         [FunctionName("GreeterScoped")]
-        public static async Task<HttpResponseMessage> Run3(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestMessage req,
+        public static async Task<IActionResult> Run3(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
             [Inject("RegisterScoped")] IGreeter greeter,
             [Inject("RegisterScoped")] IGreeterConsumer scoped)
         {
-            return req.CreateResponse($"greetersvc {greeter.Greet()} --- scopedsvc {scoped.Greeting()}");
+            return new OkObjectResult($"greetersvc {greeter.Greet()} --- scopedsvc {scoped.Greeting()}");
         }
 
         [FunctionName("GreeterNonScoped")]
-        public static async Task<HttpResponseMessage> Run4(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestMessage req,
+        public static async Task<IActionResult> Run4(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
             [Inject("RegisterNonScoped")] IGreeter greeter,
             [Inject("RegisterNonScoped")] IGreeterConsumer scoped)
         {
-            return req.CreateResponse($"greetersvc {greeter.Greet()} --- scopedsvc {scoped.Greeting()}");
+            return new OkObjectResult($"greetersvc {greeter.Greet()} --- scopedsvc {scoped.Greeting()}");
         }
 
         [FunctionName("GreeterTransient")]
-        public static async Task<HttpResponseMessage> Run5(
-           [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestMessage req,
+        public static async Task<IActionResult> Run5(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
            [Inject("RegisterTransient")] IGreeter greeter)
         {
-            return req.CreateResponse(greeter.Greet());
+            return new OkObjectResult(greeter.Greet());
         }
 
         [FunctionName("GreeterDefaultReg")]
-        public static async Task<HttpResponseMessage> Run6(
-           [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestMessage req,
+        public static async Task<IActionResult> Run6(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
            [Inject] IGreeter greeter)
         {
-            return req.CreateResponse(greeter.Greet());
+            return new OkObjectResult(greeter.Greet());
         }
 
         [FunctionName("RegisterSingletons")]
